fbda03afacdb56ca47ab54d104996c150ecbaea4
Juan Ignacio Vaccarezza <jivaccarezza@invap.com.ar>
2025-03-10 11:49:39 -0300

    Remove tmux add ssh add group

--- init-devcontainer.sh
+++ init-devcontainer.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -e
+
 if [ -z "$1" ]; then
   echo "No destination supplied"
   exit 1
@@ -10,10 +12,10 @@ script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
 DESTINATION=$1
 FINAL_DESTINATION=$DESTINATION
 
-mkdir -p $FINAL_DESTINATION
+mkdir $FINAL_DESTINATION
 
-cp -rf ${script_dir}/runs $FINAL_DESTINATION
-cp -f $script_dir/Dockerfile $FINAL_DESTINATION
-cp -f $script_dir/compose.yaml $FINAL_DESTINATION
-cp -f $script_dir/run.sh $FINAL_DESTINATION
-cp -f $script_dir/.env $FINAL_DESTINATION
+cp ${script_dir}/runs $FINAL_DESTINATION
+cp $script_dir/Dockerfile $FINAL_DESTINATION
+cp $script_dir/compose.yaml $FINAL_DESTINATION
+cp $script_dir/run.sh $FINAL_DESTINATION
+cp $script_dir/.env $FINAL_DESTINATION

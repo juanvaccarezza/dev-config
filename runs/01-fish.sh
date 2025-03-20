1f6ab6b0e16a4a048e9e1e92aa24850138978165
Juan Ignacio Vaccarezza <jivaccarezza@invap.com.ar>
2025-03-20 14:25:54 -0300

    Add fish and zk config

---
+++ runs/01-fish.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+sudo add-apt-repository ppa:fish-shell/release-4
+sudo apt install fish
+
+chsh -s $(which fish)
+
+sudo apt install exa
+
+curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher install jorgebucaran/fisher

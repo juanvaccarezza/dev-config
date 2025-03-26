38e048036609ab03b9b15a2d8516b43ba116c8a8
Juan Ignacio Vaccarezza <jivaccarezza@invap.com.ar>
2025-03-26 15:34:03 -0300

    only necessary nvim config left added fish

--- runs/01-fish.sh
+++ runs/01-fish.sh
@@ -8,3 +8,6 @@ chsh -s $(which fish)
 sudo apt install exa
 
 curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher install jorgebucaran/fisher
+
+fisher install PatrickF1/fzf.fish
+fisher install jorgebucaran/nvm.fish
--- runs/11-node.sh
+++ runs/11-node.sh
@@ -1,7 +1,3 @@
-#!/bin/sh
-
-set -e
-
-curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.8/install.sh | bash
+#!/bin/fish
 
 nvm install node
